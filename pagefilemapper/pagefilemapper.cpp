ca3d053bf140cd71f396d85ee6682d15b3fc338f
Tom R. Dial <dialtr@gmail.com>
2025-04-04 09:01:16 -0400

    Initial commit.

--- pagefilemapper/pagefilemapper.cpp
+++ pagefilemapper/pagefilemapper.cpp
@@ -1,20 +1,85 @@
-// pagefilemapper.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <Windows.h>
 
 #include <iostream>
+#include <memory>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class MemoryMappedBuffer {
+ public:
+  // TODO(tdial): Make static constructor that returns NULL or valid object.
+  MemoryMappedBuffer(size_t size)
+      : size_(size), mapping_(INVALID_HANDLE_VALUE), region_(nullptr) {
+    Create(size);
+  }
+
+  ~MemoryMappedBuffer() { Delete(); }
+
+  // Copy / assignment not allowed.
+  MemoryMappedBuffer(const MemoryMappedBuffer&) = delete;
+  MemoryMappedBuffer(MemoryMappedBuffer&&) = delete;
+  MemoryMappedBuffer& operator=(const MemoryMappedBuffer&) = delete;
+  MemoryMappedBuffer& operator=(MemoryMappedBuffer&&) = delete;
+
+  size_t Size() const { return size_; }
+
+  void* Region() const { return region_; }
+
+  bool IsValid() const { return region_ != nullptr; }
+
+ private:
+  // TODO(tdial): propagate the error somehow. The static constructor could
+  // return something like a StatusOr<>, for example.
+  DWORD Create(size_t size) {
+    // size_t is 64 bits. DWORDs required by CreateFileMapping() are 32 bits.
+    // So we need to split out the low and high DWORDS.
+    const DWORD low = size & 0xffffffff;
+    size >>= 32;
+    const DWORD high = size & 0xffffffff;
+
+    mapping_ = CreateFileMapping(INVALID_HANDLE_VALUE, NULL,
+                                 PAGE_READWRITE | SEC_COMMIT, high, low, NULL);
+    if (mapping_ == NULL) {
+      return GetLastError();
+    }
+
+    region_ = MapViewOfFile(mapping_, FILE_MAP_WRITE, 0, 0, 0);
+    if (!region_) {
+      DWORD lasterror = GetLastError();
+      CloseHandle(mapping_);
+      mapping_ = INVALID_HANDLE_VALUE;
+      return lasterror;
+    }
+    return 0;
+  }
+
+  void Delete() {
+    if (!region_) {
+      return;
+    }
+    UnmapViewOfFile(region_);
+    CloseHandle(mapping_);
+  }
+
+ private:
+  size_t size_;
+  HANDLE mapping_;
+  LPVOID region_;
+};
+
+int main(int argc, char* argv[]) {
+  // Allocate a memory mapped buffer using the page file as backing.
+  const size_t kSixteenGiB = (16ull * 1024ull * 1024ull * 1024ull);
+  std::unique_ptr<MemoryMappedBuffer> buf(new MemoryMappedBuffer(kSixteenGiB));
+  if (!buf->IsValid()) {
+    std::cout << "error creating mapping." << std::endl;
+    return 1;
+  }
+
+  // Write zeroes to every single byte in the region.
+  char* ptr = reinterpret_cast<char*>(buf->Region());
+  for (size_t i = 0; i < kSixteenGiB; ++i) {
+    ptr[i] = 0;
+  }
+  std::cout << "Done!" << std::endl;
+
+  return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
