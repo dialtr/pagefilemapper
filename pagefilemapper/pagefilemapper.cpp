8860b8af54f5fe6bbac67da05f10830fd929f63c
Tom R. Dial <dialtr@gmail.com>
2025-04-04 21:03:30 -0400

    Convert to have static constructor, making it impossible to create an invalid object.

--- pagefilemapper/pagefilemapper.cpp
+++ pagefilemapper/pagefilemapper.cpp
@@ -3,15 +3,64 @@
 #include <iostream>
 #include <memory>
 
+// Simple class that creates a (potentially large, greater than system RAM)
+// buffer that is backed by the system's page file. To use:
+//
+// 1. Create new instance with MemoryMappedBuffer::New().
+// 1a. On success, returns valid pointer.
+// 1b. On failure, returns nullptr; if opt_error is valid, provides error.
+// 2. Call Address() to get the base address of the region
+// 3. Destroy object by calling delete.
+
+// Iff dest is a valid pointer, assign src to *dest.
+template <typename T>
+void MaybeAssign(T* dest, const T& src) {
+  if (dest) {
+    *dest = src;
+  }
+}
+
 class MemoryMappedBuffer {
  public:
-  // TODO(tdial): Make static constructor that returns NULL or valid object.
-  MemoryMappedBuffer(size_t size)
-      : size_(size), mapping_(INVALID_HANDLE_VALUE), region_(nullptr) {
-    Create(size);
+  // Static function that creates a new instance.
+  // Returns valid pointer on success.
+  // Returns nullptr on failure. If there is a failure, and if the caller
+  // passes a valid DWORD ptr in opt_error, the reason for the failure
+  // is returned in opt_error.
+  static MemoryMappedBuffer* New(size_t size, DWORD* opt_error) {
+    const DWORD low = (size & 0xffffffff);
+    const DWORD high = ((size >> 32) & 0xffffffff);
+
+    // Create the file mapping.
+    HANDLE mapping = CreateFileMapping(
+        INVALID_HANDLE_VALUE,  // No file, this means: back with the page file
+        NULL,                  // No special security attributes
+        PAGE_READWRITE |
+            SEC_COMMIT,  // We want read/write, and already committed
+        high,    // The size of the mapping is specified in two 32-bit DWORDS
+        low,     // for historical reasons. We extracted these above.
+        nullptr  // No name for the file mapping
+    );
+
+    if (mapping == NULL) {
+      MaybeAssign(opt_error, GetLastError());
+      return nullptr;
+    }
+
+    LPVOID region = MapViewOfFile(mapping, FILE_MAP_WRITE, 0, 0, 0);
+    if (!region) {
+      CloseHandle(mapping);
+      MaybeAssign(opt_error, GetLastError());
+      return nullptr;
+    }
+    return new MemoryMappedBuffer(size, mapping, region);
   }
 
-  ~MemoryMappedBuffer() { Delete(); }
+  // Destructor: Unmap the view and close the underlying file mapping.
+  ~MemoryMappedBuffer() {
+    UnmapViewOfFile(address_);
+    CloseHandle(mapping_);
+  }
 
   // Copy / assignment not allowed.
   MemoryMappedBuffer(const MemoryMappedBuffer&) = delete;
@@ -19,66 +68,39 @@ class MemoryMappedBuffer {
   MemoryMappedBuffer& operator=(const MemoryMappedBuffer&) = delete;
   MemoryMappedBuffer& operator=(MemoryMappedBuffer&&) = delete;
 
+  // Return size of mapping.
   size_t Size() const { return size_; }
 
-  void* Region() const { return region_; }
-
-  bool IsValid() const { return region_ != nullptr; }
+  // Return the base address of the mapped file.
+  void* Address() const { return address_; }
 
  private:
-  // TODO(tdial): propagate the error somehow. The static constructor could
-  // return something like a StatusOr<>, for example.
-  DWORD Create(size_t size) {
-    // size_t is 64 bits. DWORDs required by CreateFileMapping() are 32 bits.
-    // So we need to split out the low and high DWORDS.
-    const DWORD low = size & 0xffffffff;
-    size >>= 32;
-    const DWORD high = size & 0xffffffff;
-
-    mapping_ = CreateFileMapping(INVALID_HANDLE_VALUE, NULL,
-                                 PAGE_READWRITE | SEC_COMMIT, high, low, NULL);
-    if (mapping_ == NULL) {
-      return GetLastError();
-    }
-
-    region_ = MapViewOfFile(mapping_, FILE_MAP_WRITE, 0, 0, 0);
-    if (!region_) {
-      DWORD lasterror = GetLastError();
-      CloseHandle(mapping_);
-      mapping_ = INVALID_HANDLE_VALUE;
-      return lasterror;
-    }
-    return 0;
-  }
-
-  void Delete() {
-    if (!region_) {
-      return;
-    }
-    UnmapViewOfFile(region_);
-    CloseHandle(mapping_);
-  }
+  MemoryMappedBuffer(size_t size, HANDLE mapping, LPVOID region)
+      : size_(size), mapping_(mapping), address_(region) {}
 
  private:
   size_t size_;
   HANDLE mapping_;
-  LPVOID region_;
+  LPVOID address_;
 };
 
 int main(int argc, char* argv[]) {
   // Allocate a memory mapped buffer using the page file as backing.
   const size_t kSixteenGiB = (16ull * 1024ull * 1024ull * 1024ull);
-  std::unique_ptr<MemoryMappedBuffer> buf(new MemoryMappedBuffer(kSixteenGiB));
-  if (!buf->IsValid()) {
-    std::cout << "error creating mapping." << std::endl;
+  DWORD error = 0;
+  std::unique_ptr<MemoryMappedBuffer> buf(
+      MemoryMappedBuffer::New(kSixteenGiB, &error));
+  if (!buf) {
+    std::cout << "Error " << error << " creating mapping." << std::endl;
     return 1;
   }
 
   // Write zeroes to every single byte in the region.
-  char* ptr = reinterpret_cast<char*>(buf->Region());
+  char* ptr = reinterpret_cast<char*>(buf->Address());
   for (size_t i = 0; i < kSixteenGiB; ++i) {
     ptr[i] = 0;
   }
+
   std::cout << "Done!" << std::endl;
 
   return 0;
